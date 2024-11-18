4ea61803c3a2925a48ccf0da08a1bdb62d0acd21
Artem Klishch <artemklishch@gmail.com>
2024-11-18 16:46:12 +0200

    default project

---
+++ src/main/kotlin/org/example/kotlin1/Kotlin1Application.kt
@@ -0,0 +1,11 @@
+package org.example.kotlin1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlin1Application
+
+fun main(args: Array<String>) {
+	runApplication<Kotlin1Application>(*args)
+}
