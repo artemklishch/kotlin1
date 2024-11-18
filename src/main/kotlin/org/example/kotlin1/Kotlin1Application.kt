51270ae8fdc3425940b9e1e3c158e04e76788193
Artem Klishch <artemklishch@gmail.com>
2024-11-18 16:48:54 +0200

    feat: task

--- src/main/kotlin/org/example/kotlin1/Kotlin1Application.kt
+++ src/main/kotlin/org/example/kotlin1/Kotlin1Application.kt
@@ -1,11 +1,12 @@
 package org.example.kotlin1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Kotlin1Application
 
-fun main(args: Array<String>) {
-	runApplication<Kotlin1Application>(*args)
+fun main() {
+	println("Enter your city…")
+	val city = readLine()
+	println("User lives in $city")
 }
